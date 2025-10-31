30297f3ce50825184f1e2ebdcf45c05e8d06b701
nickwilliamsdev <nickwilliamsdevstuff112358@proton.me>
2025-10-30 18:53:12 -0700

    base node

---
+++ scr/torchneat/genome/gene/node/base.py
@@ -0,0 +1,26 @@
+from ..base import BaseGene
+
+class BaseNode(BaseGene):
+
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, state, attrs, inputs, is_output_node=False):
+        raise NotImplementedError
+
+    def repr(self, state, node, precision=2, idx_width=3, func_width=8):
+        idx = node[0]
+
+        idx = int(idx)
+        return "{}(idx={:<{idx_width}})".format(
+            self.__class__.__name__, idx, idx_width=idx_width
+        )
+
+    def to_dict(self, state, node):
+        idx = node[0]
+        return {
+            "idx": int(idx),
+        }
+
+    def sympy_func(self, state, node_dict, inputs, is_output_node=False):
+        raise NotImplementedError
